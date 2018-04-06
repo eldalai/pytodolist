98b3e9f3f51597b0c727cc3d0f6e26cafeee127b
Gabriel Flores <gabrielf@eventbrite.com>
2018-04-06 15:09:15 -0300

    models & list

--- todolist/todolistapp/models.py
+++ todolist/todolistapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Priority(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+    done = models.BooleanField()
+
+    def __str__(self):
+        return '{}, priority: {}, done: {}'.format(
+            self.description,
+            self.priority,
+            self.done
+        )
