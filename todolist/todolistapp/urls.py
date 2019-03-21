20f6d259da8dbd24347021bffbebd60157bbe039
Gabriel Flores <gabrielf@eventbrite.com>
2019-03-21 15:12:35 +0100

    fix urlas

--- todolist/todolistapp/urls.py
+++ todolist/todolistapp/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    re_path('create/', views.TaskCreate.as_view(), name='task-create'),
-    re_path('', views.TaskList.as_view(), name='task-list'),
+    url('create/', views.TaskCreate.as_view(), name='task-create'),
+    url('', views.TaskList.as_view(), name='task-list'),
 ]
